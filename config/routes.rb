743a9650e43ac61ed4018483f6b547fbf99e6b0f
juan Illingworth <jpillingworth@hotmail.com>
2021-04-22 21:05:10 -0300

    Add movie model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "characters", to: "characters#index"
+  get "characters/:id", to: "characters#show", as: :character
 end
---
+++ db/migrate/20210423000317_create_movies.rb
@@ -0,0 +1,13 @@
+class CreateMovies < ActiveRecord::Migration[6.0]
+  def change
+    create_table :movies do |t|
+      t.blob :image
+      t.string :title
+      t.date :date
+      t.integer :rating
+      t.text :characters
+
+      t.timestamps
+    end
+  end
+end
