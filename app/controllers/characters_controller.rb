a2ad7d934b5a636cf9fa52bef4a927acbb53b313
juan Illingworth <jpillingworth@hotmail.com>
2021-04-22 21:25:00 -0300

    CRUD actions & routs for characters

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -6,4 +6,39 @@ class CharactersController < ApplicationController
   def show
     @character = Character.find(params[:id])
   end
+
+  def new
+    @character = Character.new
+  end
+
+  def create
+    @character = Character.new(params[:character])
+    @character.save
+
+    redirect_to character_path(@character)
+  end
+
+  def edit
+    @character = Character.find(params[:id])
+  end
+
+  def update
+    @character = Character.find(params[:id])
+    @character.update(params[:character])
+
+    redirect_to character_path(@character)
+  end
+
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+
+    redirect_to characters_path
+  end
+
+  private
+
+  def character_params
+    params.require(:restaurant).permit(:image, :name, :age, :weight, :history, :movie)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "characters", to: "characters#index"
+  get "characters/new", to: "characters#new
   get "characters/:id", to: "characters#show", as: :character
+  post "characters", to: "characters#create"
+  get "characters/:id/edit", to: "characters#edit"
+  patch "characters/:id", to: "characters#update"
+  delete "characters/:id", to: "characters#destroy"
 end
